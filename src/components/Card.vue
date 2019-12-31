d809696cc14bcdde50a63a3dbd3a81eea4a60424
yancymin <senseslofter@gmail.com>
2019-12-31 18:20:03 +0800

    side-panel, layout done

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  display: inline-block;
+  /* width: 100%;
+  min-width: 200px;
+  max-width: 300px; */
+  height: 100%;
+  background: #edf1f7;
+  border-radius: 12px;
+  /* padding: 10px; */
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,266 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="side-panel">
+      <div class="side-panel_top">
+        <img class="logo" src="../assets/logo.svg" alt srcset />
+        <!--eslint-disable -->
+        <p>Character Kit is a completely free collection of coaxial cartoon characters with 80 levels of images and more than 400 head / wearing parts, which can be arbitrarily matched with the image you want. You can use it in any design or development project. The casual style is widely applicable.</p>
+        <!--eslint-enable -->
+        <a download href>
+          <img src="../assets/Sketch_Logo.svg" alt />
+          Sketch
+        </a>
+        <a href target="_blank">
+          <img src="../assets/Figma_Logo.svg" alt />
+          Figma
+        </a>
+      </div>
+      <div class="side-panel_bottom">
+        <span>Designers</span>
+        <div class="links">
+          <a target="_blank" href>Yorkun</a>
+          <span>/</span>
+          <a target="_blank" href>Hwoma</a>
+          <span>/</span>
+          <a target="_blank" href>Yancy Min</a>
+          <span>/</span>
+          <a target="_blank" href>Charlie Liu</a>
+        </div>
+        <div class="social">
+          <a
+            href="https://www.producthunt.com/posts/ouch-illustrations?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ouch-illustrations"
+            target="_blank"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=141557&theme=light"
+              alt="Ouch! Illustrations - Free illustrations for better UX | Product Hunt Embed"
+              style="width: 250px; height: 54px;"
+              width="250px"
+              height="54px"
+            />
+          </a>
+          <a href="https://dribbble.com/geetest" target="_blank">
+            <img src="../assets/dribbble.svg" alt />
+          </a>
+          <a href="https://dribbble.com/geetest" target="_blank">
+            <img src="../assets/github.svg" alt />
+          </a>
+        </div>
+        <span>Made with ❤️ in Wuhan</span>
+        <span>
+          Copyright © 2020
+          <a href="https://geetest.design" target="_blank">G-Design</a>
+        </span>
+      </div>
+    </div>
+    <div class="container">
+      <Card v-for="n in 80" :key="n" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Card from '@/components/Card.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Card,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../style/global.scss';
+
+.home {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+
+  .side-panel {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    max-width: calc(440px - 20px);
+    width: 100%;
+    height: calc(100vh - 40px);
+    padding: 40px;
+    margin: 20px;
+
+    &_top {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      margin-top: -84px;
+
+      .logo {
+        position: relative;
+        left: 14px;
+        height: 54px;
+        margin-bottom: 32px;
+      }
+
+      p {
+        max-width: 360px;
+        text-align: center;
+        @include font(14, 400, 19, 0);
+      }
+      a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 48px;
+        border-radius: 4px;
+        text-decoration: none;
+        transition: all 0.25s ease;
+        @include font(16, 600, 16, 0);
+
+        img {
+          width: 24px;
+          margin-right: 16px;
+        }
+
+        &:nth-of-type(1) {
+          color: white;
+          background-color: $color-black-1;
+          margin-top: 80px;
+          margin-bottom: 20px;
+
+          &:hover {
+            color: $color-black-1;
+            background-color: white;
+            border: 1px solid $color-black-1;
+          }
+        }
+        &:nth-of-type(2) {
+          color: $color-black-1;
+          background-color: white;
+          border: 1px solid $color-black-1;
+
+          &:hover {
+            color: white;
+            background-color: $color-black-1;
+          }
+
+          img {
+            margin-left: -7px;
+          }
+        }
+      }
+    }
+
+    &_bottom {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+
+      span {
+        &:nth-of-type(1) {
+          margin-bottom: 10px;
+          @include font(12, 400, 12, 0);
+        }
+        &:nth-of-type(2) {
+          margin-bottom: 10px;
+          @include font(12, 400, 12, 0);
+        }
+        &:nth-of-type(3) {
+          @include font(12, 400, 12, 1);
+          a {
+            &:hover {
+              text-decoration: underline;
+            }
+          }
+        }
+      }
+
+      .links {
+        a {
+          @include font(14, 400, 14, 0);
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+
+        span {
+          margin: 0 8px;
+          @include font(14, 400, 14, 0);
+        }
+        margin-bottom: 20px;
+      }
+
+      .social {
+        display: flex;
+        align-items: center;
+        margin-bottom: 40px;
+        a {
+          height: 40px;
+          transition: all 0.25s ease;
+
+          &:nth-of-type(1) {
+            img {
+              width: 185px !important;
+              height: 40px !important;
+            }
+
+            &:hover {
+              filter: invert(1) hue-rotate(96deg) grayscale(1) contrast(1)
+                brightness(2);
+            }
+          }
+
+          &:nth-of-type(2),
+          &:nth-of-type(3) {
+            width: 40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid;
+            border-radius: 7px;
+          }
+          &:nth-of-type(2) {
+            border-color: #c32361;
+            margin: 0 10px;
+
+            &:hover {
+              background-color: rgba(217, 89, 136, 0.2);
+              border-color: transparent;
+            }
+          }
+          &:nth-of-type(3) {
+            border-color: #161516;
+
+            &:hover {
+              background-color: rgba(22, 21, 22, 0.1);
+              border-color: transparent;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .container {
+    position: absolute;
+    right: 60px;
+    width: calc(100vw - 520px);
+    /* height: auto; */
+    padding: 20px 20px 20px 0;
+    /* display: flex;
+    flex-wrap: wrap; */
+    display: grid;
+    grid-template-rows:repeat(20, 360px);
+    grid-template-columns: repeat(4, 25%);
+      /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+    grid-row-gap: 20px;
+    grid-column-gap: 20px;
+  }
+}
+</style>
